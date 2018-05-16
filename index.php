ef8e1020eef264b4f7668bb0f337ec3398f3d2fe
halbeisent <dev@halbeisen.fr>
2018-05-16 12:05:06 +0200

    Exercice 4 Partie 1 PHP Terminé et Corrigé

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="style.css">
+    <title>Exercice 4 Partie 1 PHP</title>
+  </head>
+  <body>
+  <?php
+  $name = 'Doe';
+  $age = 24;
+  $height = 1.75;
+  $isTrue = false;
+  echo $name
+  ?><br />
+  <?php
+  echo $age
+  ?><br />
+  <?php
+  echo $height
+  ?><br />
+  <?php
+  echo $isTrue ? 'true' : 'false';
+  ?>
+  </body>
+</html>
